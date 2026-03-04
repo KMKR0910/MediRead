da4d23a4196f017ffb73257f01dcd95e71eea0d7
KMKR0910 <kasunrashmika5@gmail.com>
2026-03-04 18:32:52 +0530

    completed and tested startup backend without ML part

--- Version 2.0/backend/main.py
+++ Version 2.0/backend/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, UploadFile, File
 from ocr import extract_text
+from ner_model import extract_medicine_entities
+
+
 
 app = FastAPI()
 
@@ -10,4 +13,15 @@ async def upload_prescription(file: UploadFile = File(...)):
     
     return {
         "extracted_text": text
+    }
+
+@app.post("/analyze")
+async def analyze_prescription(file: UploadFile = File(...)):
+    contents = await file.read()
+    text = extract_text(contents)
+    medicines = extract_medicine_entities(text)
+
+    return {
+        "text": text,
+        "medicines": medicines
     }
--- Version 2.0/backend/ocr.py
+++ Version 2.0/backend/ocr.py
@@ -2,6 +2,8 @@ import pytesseract
 from PIL import Image
 import io
 
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
 def extract_text(image_bytes):
     image = Image.open(io.BytesIO(image_bytes))
     text = pytesseract.image_to_string(image)
