ee4dcf11eaa3c1622909cb8323e8cdf7e4ae8706
KMKR0910 <kasunrashmika5@gmail.com>
2026-03-09 11:04:57 +0530

    updated react file ( prescription history & upload files)

---
+++ Version 2.0/frontend/mediread-ui/src/Components/PrescriptionHistory.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+
+function PrescriptionHistory() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+
+    fetch("http://127.0.0.1:8000/prescriptions")
+      .then(res => res.json())
+      .then(data => setData(data));
+
+  }, []);
+
+  return (
+
+    <div>
+
+      <h2>Prescription History</h2>
+
+      {data.map((item, index) => (
+
+        <div key={index} style={{border:"1px solid gray",margin:"10px",padding:"10px"}}>
+
+          <p><b>Text:</b> {item.raw_text}</p>
+
+          <p><b>Drugs:</b> {item.structured_data.drugs.join(", ")}</p>
+
+          <p><b>Dosages:</b> {item.structured_data.dosages.join(", ")}</p>
+
+          <p><b>Frequency:</b> {item.structured_data.frequencies.join(", ")}</p>
+
+        </div>
+
+      ))}
+
+    </div>
+
+  );
+}
+
+export default PrescriptionHistory;
