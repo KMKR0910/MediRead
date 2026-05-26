c1653723d57194f188f874f24759c140adc82100
KMKR0910 <kasunrashmika5@gmail.com>
2026-05-26 09:27:11 +0530

    Refactor project structure: remove version folders, keep stable v2 architecture

--- Version 1.0/Server/models/import spacy.py
+++
@@ -1,6 +0,0 @@
-import spacy
-nlp = spacy.load("en_ner_bc5cdr_md")
-doc = nlp("The patient was prescribed ibuprofen for headache.")
-for ent in doc.ents:
-    print(ent.text, ent.label_)
-# Expected output: ibuprofen CHEMICAL, headache DISEASE
